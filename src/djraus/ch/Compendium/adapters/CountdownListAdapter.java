77150e58dc4312f8bb37244bfe89b532b3a3cd4f
DJ Rausch <whitesoxfan5622@gmail.com>
2013-05-12 11:59:27 -0700

    Started work on EventListAdapter and Event Class. Did some work on the list row layout.

---
+++ src/djraus/ch/Compendium/adapters/CountdownListAdapter.java
@@ -0,0 +1,33 @@
+package djraus.ch.Compendium.adapters;
+
+import android.app.Activity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import djraus.ch.Compendium.R;
+import djraus.ch.Compendium.model.Event;
+
+import java.util.ArrayList;
+
+public class CountdownListAdapter extends ArrayAdapter<Event> {
+    private Activity context;
+    private ArrayList<Event> events;
+
+    public CountdownListAdapter(Activity context, ArrayList<Event> events) {
+        super(context, R.layout.event_list_row, events);
+        this.context = context;
+        this.events = events;
+    }
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        View rowView = convertView;
+        if (rowView == null) {
+            rowView = context.getLayoutInflater().inflate(R.layout.event_list_row, null);
+        }
+        TextView eventTitle = (TextView) rowView.findViewById(R.id.eventTitle);
+        TextView eventCountdown = (TextView) rowView.findViewById(R.id.eventCountdown);
+
+        return rowView;
+    }
+}
