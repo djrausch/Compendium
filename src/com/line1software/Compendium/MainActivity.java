e63e518798c1127e9c6e8fe477ac4426dfb42d58
DJ Rausch <whitesoxfan5622@gmail.com>
2013-05-11 21:50:55 -0700

    First commit

---
+++ src/com/line1software/Compendium/MainActivity.java
@@ -0,0 +1,52 @@
+package com.line1software.Compendium;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+import com.line1software.Compendium.util.WebRequest;
+
+public class MainActivity extends SherlockActivity {
+    TextView prizePool;
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        prizePool = (TextView) findViewById(R.id.prizePool);
+        new setPrizePool().execute();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add("Buy Compendium").setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                String url = "http://www.dota2.com/store/itemdetails/15162?appid=570";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+                return true;
+            }
+        }).setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private class setPrizePool extends AsyncTask<Void,Void,Integer>{
+        @Override
+        protected Integer doInBackground(Void... voids) {
+            return WebRequest.getPrizePool();
+        }
+        @Override
+        protected void onPostExecute(Integer result) {
+            prizePool.setText(WebRequest.convertMoneyToString(result));
+        }
+    }
+}
---
+++ src/com/line1software/Compendium/util/WebRequest.java
@@ -0,0 +1,64 @@
+package com.line1software.Compendium.util;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.text.NumberFormat;
+import java.util.Currency;
+import java.util.Locale;
+
+public class WebRequest {
+
+    public static String convertMoneyToString(int dollars) {
+        NumberFormat formatter = NumberFormat.getCurrencyInstance();
+        formatter.setCurrency(Currency.getInstance(Locale.US));
+
+        return formatter.format(dollars).replace(".00", "");
+    }
+
+    public static int getPrizePool() {
+        String result = null;
+        int prizePool;
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpGet httppost = new HttpGet("http://www.dota2.com/jsfeed/intlprizepool");
+        HttpResponse response = null;
+        try {
+            response = httpclient.execute(httppost);
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
+        try {
+            InputStream is = response.getEntity().getContent();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    is, "iso-8859-1"), 8);
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            is.close();
+            result = sb.toString();
+            reader.close();
+        } catch (Exception e) {
+
+        }
+
+        try {
+            JSONObject prizePoolObject = new JSONObject(result);
+            prizePool = prizePoolObject.getInt("dollars");
+            return prizePool;
+        } catch (JSONException e) {
+            return 1850230;
+        }
+    }
+}
