a490cd91ef14aad4428f57ee02280708cf917adc
DJ Rausch <whitesoxfan5622@gmail.com>
2013-05-12 18:38:50 -0700

    Comment out test code to launch event list activity.

--- src/djraus/ch/Compendium/MainActivity.java
+++ src/djraus/ch/Compendium/MainActivity.java
@@ -52,7 +52,7 @@ public class MainActivity extends SherlockActivity {
                 return true;
             }
         }).setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
-        menu.add("Event List").setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+        /*menu.add("Event List").setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
                 Intent i = new Intent();
@@ -60,7 +60,7 @@ public class MainActivity extends SherlockActivity {
                 startActivity(i);
                 return true;
             }
-        });
+        });*/
         return super.onCreateOptionsMenu(menu);
     }
 
