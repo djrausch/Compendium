7270a470eb2a71ccfe8b78d2dc0ac51a11906d3f
DJ Rausch <whitesoxfan5622@gmail.com>
2013-05-14 06:42:41 -0700

    Added a Goal 2a in preparation of Valve adding another goal before goal 3. I set it to 2,000,000 for now. Will compile and push update to Play Store once the new goal is announced.

--- src/djraus/ch/Compendium/MainActivity.java
+++ src/djraus/ch/Compendium/MainActivity.java
@@ -21,7 +21,9 @@ public class MainActivity extends SherlockActivity {
     private TextView totalSold;
     private TextView goal1Status;
     private TextView goal2Status;
+    private TextView goal2aStatus;
     private TextView goal3Status;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +32,7 @@ public class MainActivity extends SherlockActivity {
         totalSold = (TextView) findViewById(R.id.totalSold);
         goal1Status = (TextView) findViewById(R.id.statusGoal1);
         goal2Status = (TextView) findViewById(R.id.statusGoal2);
+        goal2aStatus = (TextView) findViewById(R.id.statusGoal2a);
         goal3Status = (TextView) findViewById(R.id.statusGoal3);
         new SetPrizePool(false).execute();
     }
@@ -57,7 +60,7 @@ public class MainActivity extends SherlockActivity {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
                 Intent i = new Intent();
-                i.setClass(MainActivity.this,EventList.class);
+                i.setClass(MainActivity.this, EventList.class);
                 startActivity(i);
                 return true;
             }
@@ -65,56 +68,70 @@ public class MainActivity extends SherlockActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
-    private class SetPrizePool extends AsyncTask<Void,Void,Integer>{
+    private class SetPrizePool extends AsyncTask<Void, Void, Integer> {
         private boolean update;
-        public SetPrizePool(boolean update){
+
+        public SetPrizePool(boolean update) {
             this.update = update;
         }
+
         @Override
         protected Integer doInBackground(Void... voids) {
             return Utility.getPrizePool();
         }
+
         @Override
         protected void onPostExecute(Integer result) {
             prizePool.setText(Utility.convertMoneyToString(result));
-            if(result>=2600000){
+            if (result >= 2600000) {
                 goal1Status.setText(R.string.completed);
                 goal2Status.setText(R.string.completed);
+                goal2aStatus.setText(R.string.completed);
                 goal3Status.setText(R.string.completed);
-            } else if(result>=1850000){
+            } else if (result >= 2000000) {
                 goal1Status.setText(R.string.completed);
                 goal2Status.setText(R.string.completed);
+                goal2aStatus.setText(R.string.completed);
                 goal3Status.setText(Utility.getRemainingCompendiums(2600000, result));
-            } else if(result>=1700000){
+            } else if (result >= 1850000) {
+                goal1Status.setText(R.string.completed);
+                goal2Status.setText(R.string.completed);
+                goal2aStatus.setText(Utility.getRemainingCompendiums(2000000, result));
+                goal3Status.setText(Utility.getRemainingCompendiums(2600000, result));
+            } else if (result >= 1700000) {
                 goal1Status.setText(R.string.completed);
                 goal2Status.setText(Utility.getRemainingCompendiums(1850000, result));
+                goal2aStatus.setText(Utility.getRemainingCompendiums(1850000, result));
                 goal3Status.setText(Utility.getRemainingCompendiums(2600000, result));
-            } else{
+            } else {
                 goal1Status.setText(Utility.getRemainingCompendiums(1700000, result));
                 goal2Status.setText(Utility.getRemainingCompendiums(1850000, result));
+                goal2aStatus.setText(Utility.getRemainingCompendiums(2000000, result));
                 goal3Status.setText(Utility.getRemainingCompendiums(2600000, result));
             }
             totalSold.setText(Utility.getSoldCompendiums(result) + " " + getResources().getString(R.string.compendiumsSold));
-            if(update){
+            if (update) {
                 //Toast.makeText(MainActivity.this,"Prize Pool Updated!",Toast.LENGTH_SHORT).show();
             }
         }
     }
 
     private class UpdatePrizePoolTask extends TimerTask {
-        public void run(){
+        public void run() {
             new SetPrizePool(true).execute();
         }
     }
+
     @Override
-    public void onPause(){
+    public void onPause() {
         super.onPause();
         t.cancel();
     }
+
     @Override
-    public void onResume(){
+    public void onResume() {
         super.onResume();
         t = new Timer();
-        t.schedule(new UpdatePrizePoolTask(),30000,30000);
+        t.schedule(new UpdatePrizePoolTask(), 30000, 30000);
     }
 }
--- src/djraus/ch/Compendium/WidgetReciever.java
+++ src/djraus/ch/Compendium/WidgetReciever.java
@@ -1,26 +1,24 @@
 package djraus.ch.Compendium;
 
-import djraus.ch.Compendium.R;
-import djraus.ch.Compendium.util.Utility;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.res.Resources;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.RemoteViews;
+import djraus.ch.Compendium.util.Utility;
 
 import java.text.DecimalFormat;
 
-public class WidgetReciever extends AppWidgetProvider{
-	
-	Context c = null;
+public class WidgetReciever extends AppWidgetProvider {
 
-	@Override
-	public void onEnabled(Context context) {
-		super.onEnabled(context);
-		
+    Context c = null;
+
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+
 //		c = context;
 //		Log.d("CompendiumWidget", "onPlace");
 //		new GetPrizePool().execute();
@@ -37,64 +35,75 @@ public class WidgetReciever extends AppWidgetProvider{
 //		rmViews.setTextViewText(R.id.widgetCurrentProgress, results[0]);
 //		rmViews.setTextViewText(R.id.widgetNextGoal, results[1]);
 //		rmViews.setTextViewText(R.id.widgetMoneyLeft, results[2]);
-	}
+    }
 
-	@Override
-	public void onUpdate(Context context, AppWidgetManager appWidgetManager,int[] appWidgetIds) {
-		super.onUpdate(context, appWidgetManager, appWidgetIds);
-		Log.d("CompendiumWidget", "onUpdate");
-		
-		c = context;
-		
-		for(int widgetId:appWidgetIds){
-			new GetPrizePool(widgetId, context, appWidgetManager).execute();
-		}		
-	}
-	
-	private String[] getPoolResults(int backgroundResults, Resources res){
-		int currPool = backgroundResults;
-		int nextGoal = 2600000;
-		int amountLeft = nextGoal-currPool;
-		DecimalFormat df = new DecimalFormat("#,###,##0");  //Nicely formats decimal numbers
-		
-		String[] results = {res.getString(R.string.current) + " $" + df.format(currPool), res.getString(R.string.goalWidget) + " $" + df.format(nextGoal), res.getString(R.string.remainder) + " $" + df.format(amountLeft)};  //an array of all elements to go into the widget
-		
-		return results;
-	}
-	
-	private class GetPrizePool extends AsyncTask<Void, Void, Integer>{
-		
-		Context c = null;
-		int id = 0;
-		AppWidgetManager manager = null;
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+        Log.d("CompendiumWidget", "onUpdate");
 
-		public GetPrizePool(int widgetId, Context context, AppWidgetManager appWidgetManager) {
-			c = context;
-			id = widgetId;
-			manager = appWidgetManager;
-		}
+        c = context;
 
-		@Override
-		protected Integer doInBackground(Void... params) {
-			Log.d("ConpendiumWidget", "doInBackground");
-			return Utility.getPrizePool();
-		}
-		
-		@Override
-		protected void onPostExecute(Integer result) {
-			super.onPostExecute(result);
-			Log.d("ConpendiumWidget", "onPost");
+        for (int widgetId : appWidgetIds) {
+            new GetPrizePool(widgetId, context, appWidgetManager).execute();
+        }
+    }
+
+    private String[] getPoolResults(int backgroundResults, Resources res) {
+        int currPool = backgroundResults;
+        int nextGoal = 0;
+        if (currPool >= 2600000) {
+            //Over
+        } else if (currPool >= 2000000) {
+            nextGoal = 2600000;
+        } else if (currPool >= 1850000) {
+            nextGoal = 2000000;
+        } else if (currPool >= 1700000) {
+            nextGoal = 1850000;
+        } else {
+            nextGoal = 1700000;
+        }
+        int amountLeft = nextGoal - currPool;
+        DecimalFormat df = new DecimalFormat("#,###,##0");  //Nicely formats decimal numbers
+
+        String[] results = {res.getString(R.string.current) + " $" + df.format(currPool), res.getString(R.string.goalWidget) + " $" + df.format(nextGoal), res.getString(R.string.remainder) + " $" + df.format(amountLeft)};  //an array of all elements to go into the widget
+
+        return results;
+    }
+
+    private class GetPrizePool extends AsyncTask<Void, Void, Integer> {
+
+        Context c = null;
+        int id = 0;
+        AppWidgetManager manager = null;
+
+        public GetPrizePool(int widgetId, Context context, AppWidgetManager appWidgetManager) {
+            c = context;
+            id = widgetId;
+            manager = appWidgetManager;
+        }
+
+        @Override
+        protected Integer doInBackground(Void... params) {
+            Log.d("ConpendiumWidget", "doInBackground");
+            return Utility.getPrizePool();
+        }
+
+        @Override
+        protected void onPostExecute(Integer result) {
+            super.onPostExecute(result);
+            Log.d("ConpendiumWidget", "onPost");
 //			DecimalFormat df = new DecimalFormat("#,###,##0");
-			RemoteViews rmViews = new RemoteViews(c.getPackageName(), R.layout.widget_layout);
-			String[] backgroundResults = getPoolResults(result, c.getResources());
-			
-			rmViews.setTextViewText(R.id.widgetCurrentProgress, backgroundResults[0]);
-			rmViews.setTextViewText(R.id.widgetNextGoal, backgroundResults[1]);
-			rmViews.setTextViewText(R.id.widgetMoneyLeft, backgroundResults[2]);
-			
-			manager.updateAppWidget(id, rmViews);
-		}
-		
-	}
+            RemoteViews rmViews = new RemoteViews(c.getPackageName(), R.layout.widget_layout);
+            String[] backgroundResults = getPoolResults(result, c.getResources());
+
+            rmViews.setTextViewText(R.id.widgetCurrentProgress, backgroundResults[0]);
+            rmViews.setTextViewText(R.id.widgetNextGoal, backgroundResults[1]);
+            rmViews.setTextViewText(R.id.widgetMoneyLeft, backgroundResults[2]);
+
+            manager.updateAppWidget(id, rmViews);
+        }
+
+    }
 
 }
