44928f6f6d12925449942242ea36573371544450
DJ Rausch <whitesoxfan5622@gmail.com>
2013-05-14 20:47:48 -0700

    Use new list and adapter

--- src/djraus/ch/Compendium/MainActivity.java
+++ src/djraus/ch/Compendium/MainActivity.java
@@ -1,28 +1,32 @@
 package djraus.ch.Compendium;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.widget.TextView;
-import android.widget.Toast;
 import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.app.SherlockListActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
 
+import djraus.ch.Compendium.adapters.GoalListAdapter;
+import djraus.ch.Compendium.model.Goal;
 import djraus.ch.Compendium.util.Utility;
 
+import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class MainActivity extends SherlockActivity {
+public class MainActivity extends SherlockListActivity {
     private TextView prizePool;
     private Timer t;
     private TextView totalSold;
-    private TextView goal1Status;
-    private TextView goal2Status;
-    private TextView goal2aStatus;
-    private TextView goal3Status;
+    private GoalListAdapter goalListAdapter;
+    private int currentPrizePool;
+    private ArrayList<Goal> goals = new ArrayList<Goal>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,10 +34,19 @@ public class MainActivity extends SherlockActivity {
         setContentView(R.layout.main);
         prizePool = (TextView) findViewById(R.id.prizePool);
         totalSold = (TextView) findViewById(R.id.totalSold);
-        goal1Status = (TextView) findViewById(R.id.statusGoal1);
-        goal2Status = (TextView) findViewById(R.id.statusGoal2);
-        goal2aStatus = (TextView) findViewById(R.id.statusGoal2a);
-        goal3Status = (TextView) findViewById(R.id.statusGoal3);
+        goalListAdapter = new GoalListAdapter(MainActivity.this, goals, currentPrizePool);
+        this.setListAdapter(goalListAdapter);
+        Resources r = getResources();
+        int px15 = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                15, r.getDisplayMetrics());
+        int px10 = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                10, r.getDisplayMetrics());
+
+        getListView().setPadding(px15, px10, px15, px10);
+        getListView().setDivider(this.getResources().getDrawable(
+                android.R.color.transparent));
+        getListView().setDividerHeight(px10);
+        getListView().setVerticalScrollBarEnabled(false);
         new SetPrizePool(false).execute();
     }
 
@@ -77,42 +90,22 @@ public class MainActivity extends SherlockActivity {
 
         @Override
         protected Integer doInBackground(Void... voids) {
-            return Utility.getPrizePool();
+            goals.add(new Goal("Goal 1", 1700000, "Compendium owners receive a Limited Edition 125% Battle Booster that lasts until The International ends."));
+            goals.add(new Goal("Goal 2", 1850000, "Compendium Courier gains additional stages."));
+            goals.add(new Goal("Goal 3", 2000000, "Compendium owners will receive a custom HUD skin."));
+            goals.add(new Goal("Goal 4", 2200000, "Compendium owners will receive a Taunt item with a brand new animation."));
+            goals.add(new Goal("Goal 5", 2400000, "Compendium owners get to vote on participants in an 8 player Solo Championship (1 vs 1) at The International."));
+            goals.add(new Goal("Goal 6", 2600000, "Compendium owners will receive a new Immortal item."));
+            goals.add(new Goal("Goal 7", 3200000, "Compendium Owners will be able to select the next Hero shipped in Dota 2."));
+            currentPrizePool = Utility.getPrizePool();
+            return currentPrizePool;
         }
 
         @Override
-        protected void onPostExecute(Integer result) {
-            prizePool.setText(Utility.convertMoneyToString(result));
-            if (result >= 2600000) {
-                goal1Status.setText(R.string.completed);
-                goal2Status.setText(R.string.completed);
-                goal2aStatus.setText(R.string.completed);
-                goal3Status.setText(R.string.completed);
-            } else if (result >= 2000000) {
-                goal1Status.setText(R.string.completed);
-                goal2Status.setText(R.string.completed);
-                goal2aStatus.setText(R.string.completed);
-                goal3Status.setText(Utility.getRemainingCompendiums(2600000, result));
-            } else if (result >= 1850000) {
-                goal1Status.setText(R.string.completed);
-                goal2Status.setText(R.string.completed);
-                goal2aStatus.setText(Utility.getRemainingCompendiums(2000000, result));
-                goal3Status.setText(Utility.getRemainingCompendiums(2600000, result));
-            } else if (result >= 1700000) {
-                goal1Status.setText(R.string.completed);
-                goal2Status.setText(Utility.getRemainingCompendiums(1850000, result));
-                goal2aStatus.setText(Utility.getRemainingCompendiums(1850000, result));
-                goal3Status.setText(Utility.getRemainingCompendiums(2600000, result));
-            } else {
-                goal1Status.setText(Utility.getRemainingCompendiums(1700000, result));
-                goal2Status.setText(Utility.getRemainingCompendiums(1850000, result));
-                goal2aStatus.setText(Utility.getRemainingCompendiums(2000000, result));
-                goal3Status.setText(Utility.getRemainingCompendiums(2600000, result));
-            }
-            totalSold.setText(Utility.getSoldCompendiums(result) + " " + getResources().getString(R.string.compendiumsSold));
-            if (update) {
-                //Toast.makeText(MainActivity.this,"Prize Pool Updated!",Toast.LENGTH_SHORT).show();
-            }
+        protected void onPostExecute(Integer currentPrizePool) {
+            prizePool.setText(Utility.convertMoneyToString(currentPrizePool));
+            totalSold.setText(Utility.getSoldCompendiums(currentPrizePool) + " " + getResources().getString(R.string.compendiumsSold));
+            goalListAdapter.updateCurrentPrizePool(currentPrizePool);
         }
     }
 
